bece7fa8ba7805387cf56d14b0b7e6f04da25adc
unknown <raeburn.trent6@gmail.com>
2023-03-22 23:25:51 -0400

    Sidebar fin

--- my-portfolio/src/componenets/Sidebar/index.js
+++ my-portfolio/src/componenets/Sidebar/index.js
@@ -27,6 +27,18 @@ const Sidebar = () => {
         <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
     </NavLink>
     </nav>
+    <ul>
+        <li>
+        <a href='https://www.linkedin.com/in/traeburn058/' target='_blank' rel='noreferrer'>
+            <i className='fa-brands fa-linkedin' ></i>
+        </a>
+        </li>
+        <li>
+        <a href='https://github.com/TrentLe' target='_blank' rel='noreferrer'>
+            <i className='fa-brands fa-github' ></i>
+        </a>
+        </li>
+        </ul>
   </div>
   )
 
