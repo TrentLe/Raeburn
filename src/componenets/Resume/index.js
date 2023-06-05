61448894b05affb630104ca54f17c048349a3335
jahnathan x <jahnathanexantus@gmail.com>
2023-06-05 19:20:10 -0400

    fixed resume

--- src/componenets/Resume/index.js
+++ src/componenets/Resume/index.js
@@ -1,35 +1,25 @@
-import React, {state} from 'react';
+import React from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 import './index.scss';
 import FinalResume from '../../assets/document/FinalResume.pdf';
-import resume from '../../assets/document/resume.docx';
-import DocViewer from "react-doc-viewer";
 
-// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const Resume = () => {
+  const file = FinalResume;
 
-    const file = FinalResume;
-
- return (
+  return (
     <section className="resume">
-        <Document file={file}>
-            <Page pageNumber={1} />
-            <br />
-            <br />
-            <br />
-            <br />
-
-            <Page pageNumber={2} />
-        </Document>
-   
-    
+      <Document file={file}>
+        <Page pageNumber={1} />
+        <br />
+        <br />
+        <br />
+        <br />
+        <Page pageNumber={2} />
+      </Document>
     </section>
-    
-    
-    )
-
-   
-}
+  );
+};
 
 export default Resume;
